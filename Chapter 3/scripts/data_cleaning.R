2a3c2b7a4701970aad5cc0f5f0655936d784e093
Maya Dalton <124836305+mdalton21@users.noreply.github.com>
2026-04-29 15:22:52 -0400

    Update data_cleaning.R

--- Chapter 3/scripts/data_cleaning.R
+++ Chapter 3/scripts/data_cleaning.R
@@ -7,8 +7,6 @@
 ##
 ## -----------------------------------------------------------------------------
 
-rm(list=ls())
-
 ## libraries
 libs <- c("haven", "readxl", "dplyr", "tidyverse", "ggplot2")
 sapply(libs, require, character.only = TRUE)
@@ -17,9 +15,11 @@ sapply(libs, require, character.only = TRUE)
 args <- commandArgs(trailingOnly = TRUE)
 root <- ifelse(length(args) == 0, file.path(".."), args)
 dat_dir <- file.path(root, "data")
-fig_dir <- file.path(root, "figures")
-fig_path <- "~/Dropbox/Apps/Overleaf/Dalton EV Quotas (CH. 3)/Figures"
-tab_path <- "~/Dropbox/Apps/Overleaf/Dalton EV Quotas (CH. 3)/Tables"
+dir.create(dat_dir, recursive = TRUE, showWarnings = FALSE)
+fig_path <- file.path(root, "figures")
+tab_path <- file.path(root, "tables")
+dir.create(fig_path, recursive = TRUE, showWarnings = FALSE)
+dir.create(tab_path, recursive = TRUE, showWarnings = FALSE)
 
 ## load datasets
 ecav <- read_excel(file.path(dat_dir, "ecav.xls"))
